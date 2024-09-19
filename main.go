0b0654cd87c24459d02fdb8d09c9d6b33bb9e2d4
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2024-09-19 08:41:11 -0300

    feat: fiber api

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 
@@ -29,6 +31,13 @@ type Game struct {
 	Slug string `json:"slug" db:"slug"`
 }
 
+type DbGame struct {
+	Id    string `json:"id" db:"id"`
+	Name  string `json:"name" db:"name"`
+	Slug  string `json:"slug" db:"slug"`
+	ApiId int    `json:"api_id" db:"api_id"`
+}
+
 type EnvVars struct {
 	TWITCH_CLIENT_ID  string
 	TWITCH_SECRET     string
@@ -41,6 +50,8 @@ type EnvVars struct {
 type CountResponse struct {
 	Count int `json:"count" db:"count"`
 }
+
+var db *sqlx.DB
 
 func loadEnv() EnvVars {
 	err := godotenv.Load()
@@ -58,71 +69,73 @@ func loadEnv() EnvVars {
 	}
 }
 
-func getAuthToken(twitchClientId, twitchSecret string) AuthResponse {
-	authResponse := AuthResponse{}
+func getAuthToken(twitchClientId, twitchSecret string) (*AuthResponse, error) {
+	authResponse := &AuthResponse{}
 	authUrl := fmt.Sprintf("https://id.twitch.tv/oauth2/token?client_id=%s&client_secret=%s&grant_type=client_credentials", twitchClientId, twitchSecret)
 	resp, err := http.Post(authUrl, "application/json", nil)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
 	err = json.Unmarshal(body, &authResponse)
 	if err != nil {
-
-		log.Fatalln(err)
+		return nil, err
 	}
 
-	return authResponse
+	return authResponse, nil
 }
 
-func getGames(limit, offset int, accessToken, clientId string, httpClient *http.Client) []Game {
-	jsonString := fmt.Sprintf("fields id, name, slug; sort id asc; limit %d; offset %d;", limit, offset)
+func getGames(limit, offset int, accessToken, clientId string, httpClient *http.Client) ([]Game, error) {
+	jsonString := fmt.Sprintf("fields id, name, slug; sort created_at asc; limit %d; offset %d;", limit, offset)
 	jsonBytes := []byte(jsonString)
 	req, err := http.NewRequest("POST", "https://api.igdb.com/v4/games", bytes.NewBuffer(jsonBytes))
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 	req.Header.Set("Client-ID", clientId)
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
 	req.Header.Set("Accept", "application/json")
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
 	gamesResponse := []Game{}
 
 	err = json.Unmarshal(body, &gamesResponse)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
-	return gamesResponse
+	return gamesResponse, err
 }
 
-func getTotalGamesDb(db *sqlx.DB) int {
+func getTotalGamesDb(db *sqlx.DB) (int, error) {
 	countResponse := CountResponse{}
-	db.Get(&countResponse, "SELECT count(id) FROM games")
+	err := db.Get(&countResponse, "SELECT count(id) FROM games")
+	if err != nil {
+		return 0, err
+	}
 
-	return countResponse.Count
+	return countResponse.Count, nil
 }
 
-func getTotalGamesApi(accessToken, clientId string, httpClient *http.Client) int {
+func getTotalGamesApi(accessToken, clientId string, httpClient *http.Client) (int, error) {
 	req, err := http.NewRequest("POST", "https://api.igdb.com/v4/games/count", nil)
 	if err != nil {
-		log.Fatalln(err)
+		return 0, err
 	}
 
 	req.Header.Set("Client-ID", clientId)
@@ -130,73 +143,159 @@ func getTotalGamesApi(accessToken, clientId string, httpClient *http.Client) int
 	req.Header.Set("Accept", "application/json")
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		log.Fatalln(err)
+		return 0, err
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return 0, err
 	}
 
 	countResponse := CountResponse{}
 
 	err = json.Unmarshal(body, &countResponse)
 	if err != nil {
-		log.Fatalln(err)
+		return 0, err
 	}
 
-	return countResponse.Count
+	return countResponse.Count, nil
+}
+
+type DbCredentials struct {
+	User     string
+	Database string
+	Host     string
+	Password string
+}
+
+func Connect() error {
+	env := loadEnv()
+
+	connectionString := fmt.Sprintf("user=%s dbname=%s sslmode=disable password=%s host=%s", env.DATABASE_USER, env.DATABASE_DB, env.DATABASE_PASSWORD, env.DATABASE_HOST)
+
+	var err error
+	db, err = sqlx.Connect("postgres", connectionString)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func importGames(env EnvVars) error {
+        authResponse, err := getAuthToken(env.TWITCH_CLIENT_ID, env.TWITCH_SECRET)
+		if err != nil {
+            return err
+		}
+		client := &http.Client{}
+
+		dbCount, err := getTotalGamesDb(db)
+		if err != nil {
+            return err
+		}
+		apiCount, err := getTotalGamesApi(authResponse.AccessToken, env.TWITCH_CLIENT_ID, client)
+		if err != nil {
+            return err
+		}
+
+		if dbCount == apiCount {
+            return nil
+		}
+
+		if dbCount > apiCount {
+            return err
+		}
+
+		missingGamesQt := apiCount - dbCount
+		limit := 500
+		callsUntilFinish := int(math.Ceil(float64(missingGamesQt) / float64(limit)))
+
+		offset := dbCount
+		lastTime := time.Now().Add(-250 * 1e6)
+
+		for i := 0; i < callsUntilFinish; i++ {
+			executionDuration := time.Since(lastTime).Milliseconds()
+			if executionDuration < 250 {
+				difference := 250 - executionDuration
+
+				time.Sleep(time.Duration(difference * 1e6))
+			}
+
+			games, err := getGames(limit, offset, authResponse.AccessToken, env.TWITCH_CLIENT_ID, client)
+			if err != nil {
+                return err
+			}
+			lastTime = time.Now()
+
+			_, err = db.NamedExec("INSERT INTO games (api_id, name, slug) VALUES (:id, :name, :slug)", games)
+
+			if err != nil {
+				log.Printf("Stopped at offset %d\n", offset)
+                return err
+			}
+
+			offset += 500
+		}
+
+        return nil
 }
 
 func main() {
 	env := loadEnv()
 
-	connectionString := fmt.Sprintf("user=%s dbname=%s sslmode=disable password=%s host=%s", env.DATABASE_USER, env.DATABASE_DB, env.DATABASE_PASSWORD, env.DATABASE_HOST)
-	db, err := sqlx.Connect("postgres", connectionString)
-	if err != nil {
-		log.Fatalln(err)
+	if err := Connect(); err != nil {
+		log.Fatal(err)
 	}
 
-	authResponse := getAuthToken(env.TWITCH_CLIENT_ID, env.TWITCH_SECRET)
-	client := &http.Client{}
+	app := fiber.New()
 
-	dbCount := getTotalGamesDb(db)
-	apiCount := getTotalGamesApi(authResponse.AccessToken, env.TWITCH_CLIENT_ID, client)
+    app.Use(logger.New())
 
-	if dbCount == apiCount {
-		return
-	}
+	api := app.Group("/api")
 
-	if dbCount > apiCount {
-		panic("Something went wrong")
-	}
+	v1 := api.Group("/v1")
 
-	missingGamesQt := apiCount - dbCount
-	limit := 500
-	callsUntilFinish := int(math.Ceil(float64(missingGamesQt) / float64(limit)))
+	v1.Get("/games/:slug", func(c *fiber.Ctx) error {
+		game := DbGame{}
+		slug := c.Params("slug")
 
-	offset := dbCount
-	lastTime := time.Now().Add(-250 * 1e6)
-
-	for i := 0; i < callsUntilFinish; i++ {
-		executionDuration := time.Since(lastTime).Milliseconds()
-		if executionDuration < 250 {
-			difference := 250 - executionDuration
-
-			time.Sleep(time.Duration(difference * 1e6))
-		}
-
-		games := getGames(limit, offset, authResponse.AccessToken, env.TWITCH_CLIENT_ID, client)
-		lastTime = time.Now()
-
-		_, err = db.NamedExec("INSERT INTO games (api_id, name, slug) VALUES (:id, :name, :slug)", games)
-
+		err := db.Get(&game, "SELECT * FROM games WHERE games.slug = $1", slug)
 		if err != nil {
-			fmt.Printf("Stopped at offset %d\n", offset)
-			log.Fatalln(err)
+			log.Println(err)
+			return c.Status(fiber.StatusInternalServerError).SendString(fiber.ErrInternalServerError.Message)
 		}
 
-		offset += 500
-	}
+		if game.Id == "" {
+			return c.Status(fiber.StatusNotFound).SendString(fiber.ErrNotFound.Message)
+		}
+
+		return c.JSON(game)
+	})
+	v1.Post("/ratings/", func(c *fiber.Ctx) error {
+		type Body struct {
+			GameId string `json:"game_id"`
+			UserId string `json:"user_id"`
+			Rate   int    `json:"rate"`
+		}
+		u := new(Body)
+
+		if err := c.BodyParser(u); err != nil {
+			return c.Status(fiber.StatusUnprocessableEntity).SendString(err.Error())
+		}
+
+		_, err := db.Exec("INSERT INTO ratings (game_id, user_id, rate) VALUES ($1, $2, $3)", u.GameId, u.UserId, u.Rate)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(fiber.ErrInternalServerError.Message)
+		}
+
+		return c.Status(fiber.StatusCreated).SendString("Ok")
+	})
+	v1.Post("/games/import", func(c *fiber.Ctx) error {
+        go importGames(env)
+
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	app.Listen(":3000")
 }
---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS games (
+    id VARCHAR(255) PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    slug TEXT UNIQUE,
+    api_id INTEGER NOT NULL UNIQUE
+)
+
+CREATE TABLE IF NOT EXISTS users (
+    id VARCHAR(255) PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS ratings (
+    id VARCHAR(255) PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    user_id VARCHAR(255) REFERENCES users(id),
+    game_id VARCHAR(255) REFERENCES games(id),
+    rate INTEGER NOT NULL
+)
